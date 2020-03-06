4e5318c150af89506e759493187e0b9910e66034
Gonzako <al341522@uji.es>
2020-03-06 21:49:17 +0100

    added More gameplay physics layers

---
+++ Assets/Scripts/Pickables/DefaultPickable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultPickable : MonoBehaviour, BasePickable
+{
+    public void onHighlight()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void onPickup()
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
