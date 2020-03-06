b516a053874af0c82f4ba00ccb50abb3a86543f0
Daryoush Farsimadan <farsimadan.daryoush@gmail.com>
2020-03-06 22:34:01 +0200

    Import reusable  code

---
+++ Assets/Scripts/AI/AIManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.AI;
+
+public class AIManager : MonoBehaviour
+{
+    private Dictionary<Type, BaseAIState> _initialStates;
+    private AIStateManager _stateManager;
+    [SerializeField] private Transform _target;
+    public Rigidbody2D _rb;
+    public NavMeshAgent _agent;
+    public int _team;
+
+    
+    public Transform target { get { return _target; } set{ target = value; }}
+
+    [SerializeField] public AISettings _settings;
+
+    private void OnEnable()
+    {
+        SetupStates();
+    }
+
+    private void Start()
+    {
+        SetupStates();
+
+        _rb = GetComponent<Rigidbody2D>();
+        _agent =  GetComponent<NavMeshAgent>();
+    }
+
+    private void SetupStates()
+    {
+        _stateManager = GetComponent<AIStateManager>();
+        _initialStates = new Dictionary<Type, BaseAIState>
+        {
+       
+        };
+        _stateManager.SetStates(_initialStates);
+    }
+
+    private void Update()
+    {
+   
+    }
+
+}
+
