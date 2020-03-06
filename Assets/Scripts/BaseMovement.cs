533a529c3a5cd8b09fbbdc3f69b024a28d9d974a
Gonzako <al341522@uji.es>
2020-03-06 20:13:11 +0100

    added 2ddevSprites

---
+++ Assets/Scripts/BaseMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+
+
+    private Rigidbody2D rb;
+    private Vector2 inputVector;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        inputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+        if (inputVector.magnitude == 0) rb.velocity = Vector2.zero;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = inputVector * speed;
+        inputVector = Vector2.zero;
+    }
+
+
+
+}
