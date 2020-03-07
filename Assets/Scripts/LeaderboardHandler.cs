0caec106a83f26df8573960e9b39403d72e7c6d9
Mark Christian Calugay <mark.calugay@gmail.com>
2020-03-07 13:59:34 -0600

    Creates Leaderboard prefab

---
+++ Assets/Scripts/LeaderboardHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeaderboardHandler : MonoBehaviour
+{
+    public GameObject PrisonerID;
+    public GameObject NumberOfFollowers;
+}
